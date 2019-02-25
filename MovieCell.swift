d46994530839fd3e6ce6e353bcfbdc4866677ab6
yabutaka <tacky0630@gmail.com>
2019-02-25 12:36:49 -0800

    Assignment 2: This is actually the second try

---
+++ MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by 薮田隆司 on 12/02/2019.
+//  Copyright © 2019 Takashi.Co. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
